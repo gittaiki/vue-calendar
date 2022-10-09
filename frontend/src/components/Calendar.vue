0a12e258d73d81b974dd5bb5da6e08f6fcec4df5
gittaiki <0215.taiki@gmail.com>
2022-10-09 23:30:06 +0900

    2~3章

---
+++ frontend/src/components/Calendar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Calendar</h1>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+      </li>
+    </ul>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+    <CalendarDetails />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import CalendarDetails from './CalendarDetails.vue';
+
+export default {
+  name: 'Calendar',
+  // importしたCalendarDetailsをHTMLで描画するために必要
+  components: {
+    CalendarDetails,
+  },
+  // ステートの値が変わると発火
+  computed: {
+    // getterrsのevents関数を動作させる
+    ...mapGetters('events', ['events']),
+  },
+  methods: {
+    // actionsのfetchEvents関数を動作させる
+    ...mapActions('events', ['fetchEvents'])
+  },
+};
+</script>
---
+++ frontend/src/store/modules/events.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:3000';
+
+const state = {
+  events: []
+};
+
+// computedのmapGettersから呼び出される
+const getters = {
+  events: state => state.events
+}
+
+const mutations = {
+  // eventsステートの値をRails APIのレスポンスデータで上書き(更新)する
+  setEvents: (state, events) => (state.events = events)
+}
+
+// methodsのmapActionsから呼び出される
+const actions = {
+  // asyncは非同期関数を定義する関数宣言のこと
+  async fetchEvents({ commit }) {
+    // 関数の前にawaitを指定すると、その関数の結果が返されるまで待機
+    const response = await axios.get(`${apiUrl}/events`);
+    commit('setEvents', response.data);  // mutationを呼び出す
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+};
